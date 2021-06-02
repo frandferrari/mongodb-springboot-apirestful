11d4b4ec494b25d6023f059f7961b472d4f32112
frandferrari <frandferrari@outlook.com>
2021-06-02 14:28:27 -0300

    Getting a user by Id

--- src/main/java/com/ferrari/wsmongo/services/UserService.java
+++ src/main/java/com/ferrari/wsmongo/services/UserService.java
@@ -1,20 +1,27 @@
 package com.ferrari.wsmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ferrari.wsmongo.domain.User;
 import com.ferrari.wsmongo.repository.UserRepository;
+import com.ferrari.wsmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
