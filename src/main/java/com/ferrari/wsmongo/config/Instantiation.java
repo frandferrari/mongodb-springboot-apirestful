d365eb3f8c7e814a88707223fd650a7f3151f958
frandferrari <frandferrari@outlook.com>
2021-06-02 16:39:40 -0300

    Projection of author data with DTO

--- src/main/java/com/ferrari/wsmongo/config/Instantiation.java
+++ src/main/java/com/ferrari/wsmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.ferrari.wsmongo.domain.Post;
 import com.ferrari.wsmongo.domain.User;
+import com.ferrari.wsmongo.dto.AuthorDTO;
 import com.ferrari.wsmongo.repository.PostRepository;
 import com.ferrari.wsmongo.repository.UserRepository;
 
@@ -35,10 +36,11 @@ public class Instantiation implements CommandLineRunner{
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem!", "Vou viajar pra São Paulo, abraços.", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje", maria);
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem!", "Vou viajar pra São Paulo, abraços.", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje", new AuthorDTO(maria));
+	
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
